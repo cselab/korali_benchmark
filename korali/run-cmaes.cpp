9cd1f42d0fbe16ffe315979b87d90d29f9e2f336
Sergio Martin <martiser@ethz.ch>
2020-09-15 18:06:25 +0200

    progress on jobs

--- korali/run-cmaes.cpp
+++ korali/run-cmaes.cpp
@@ -2,37 +2,55 @@
 
 void model(korali::Sample &k)
 {
-  srand(k["Sample Id"]); // seeding randomizer with sample id
-  int base = 750;
-  int variance = rand()%500;
-  int waitTime = (base + variance)*1000;
+  system("../common/waitScript.sh");
 
-  //printf("Random Number: %d\n", variance);
-  usleep(waitTime);
- 
-  k["F(x)"] = variance; 
+  srand(k["Sample Id"]); // seeding randomizer with sample id
+  k["F(x)"] = (double)rand() / 100000.0;
 }
 
 int main(int argc, char *argv[])
 {
-  auto k = korali::Engine();
-  auto e = korali::Experiment();
+ int n  = 1; // Number of samples per generation
+ int g  = 1; // Number of generations
+ int nE = 1; // Number of experiments
 
-  e["Problem"]["Type"] = "Optimization";
-  e["Problem"]["Objective Function"] = &model;
+  for (int i = 1; i < argc; i++)
+  {
+   bool recognized = false;
 
-  e["Solver"]["Type"] = "Optimizer/CMAES";
-  e["Solver"]["Population Size"] = 256;
-  e["Solver"]["Termination Criteria"]["Max Generations"] = 5;
+   if(!strcmp(argv[i], "-g")) { g = atoi(argv[++i]); recognized = true; }
+   if(!strcmp(argv[i], "-n")) { n = atoi(argv[++i]); recognized = true; }
+   if(!strcmp(argv[i], "-e")) { nE = atoi(argv[++i]); recognized = true; }
 
-  e["Variables"][0]["Name"] = "X";
-  e["Variables"][0]["Lower Bound"] = -3.0;
-  e["Variables"][0]["Upper Bound"] = +3.0;
-
-  e["File Output"]["Enabled"] = false;
+   if (recognized == false)
+   {
+    fprintf(stderr, "[Error] Unrecognized parameter: %s\n", argv[i]);
+    exit(-1);
+   }
+  }
   
+  std::vector<korali::Experiment> eList(nE);
+  auto k = korali::Engine();
+
+  for(size_t i = 0; i < nE; i++)
+  {
+
+   eList[i]["Problem"]["Type"] = "Optimization";
+   eList[i]["Problem"]["Objective Function"] = &model;
+
+   eList[i]["Solver"]["Type"] = "Optimizer/CMAES";
+   eList[i]["Solver"]["Population Size"] = n;
+   eList[i]["Solver"]["Termination Criteria"]["Max Generations"] = g;
+
+   eList[i]["Variables"][0]["Name"] = "X";
+   eList[i]["Variables"][0]["Lower Bound"] = -3.0;
+   eList[i]["Variables"][0]["Upper Bound"] = +3.0;
+
+   eList[i]["File Output"]["Enabled"] = false;
+  }  
+
   k["Conduit"]["Type"] = "Distributed";
-  k.run(e);
+  k.run(eList);
 
   return 0;
 }
--- pi4u/fitfun.c
+++ pi4u/fitfun.c
@@ -1,10 +1,15 @@
-#include <math.h>
-#include "../common/waitFunction.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 double fitfun(double /*const*/ *x, int N, void *output, int *info)
 {
-   int sampleId = info[3];
-   double result = waitFunction(sampleId);
-   return result;
+   int sampleId = info[0]*4096 + info[3];
+
+   system("../common/waitScript.sh");
+
+   // Generating random result
+   srand(sampleId);
+   return (double)rand() / 100000.0;
 }
 
